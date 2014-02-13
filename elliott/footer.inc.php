ef581c7fad308c3540179c113230a13f24da57ff
George Dewar <george@dewar.co.nz>
2014-02-13 20:57:02 +1300

    Remove old stuff

--- elliott/footer.inc.php
+++
@@ -1,7 +0,0 @@
-<div class="clear" style="height:30px;"></div>
-    </div>
-  </div>
-  <div id="footer">&copy; Copyright <?php echo date("Y"); ?>, Elliot Engineering Ltd. Structural and Sheetmetal Engineers</div>
-</div>
-</body>
-</html>
